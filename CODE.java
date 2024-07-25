ed522cee9a78ed5bec59b74ef867571d77a1ff71
Aditya Chopra <110564061+Aditya-Chopra30@users.noreply.github.com>
2024-07-25 19:08:30 +0530

    Update CODE.java

--- CODE.java
+++ CODE.java
@@ -1,32 +1,82 @@
 import java.util.*;
-class crs 
+class chainreaction
 {
-public static void main(String at[])throws Exception//main function starts
-{//welcome screen
-System.out.println("        #           #  #######  #       ######  ######  ##   ##  ###### ");
-System.out.println("         #         #   #        #       #       #    #  # # # #  #      ");
-System.out.println("          #   #   #    #####    #       #       #    #  #  #  #  ####   ");
-System.out.println("           # # # #     #        #       #       #    #  #     #  #      ");                                                                   
-System.out.println("            #   #      #######  ######  ######  ######  #     #  ###### ");
+public static void main(String at[])throws Exception
+{
+//welcome screen
+
+for(int th=1;th<=18;th++)
+{
+System.out.println(" ");
+}
+System.out.println("       ");
+Thread.sleep(76);
+System.out.println("       #              #  ######  #       ###### ###### ##     ##  ######  ");
+Thread.sleep(76);                     
+System.out.println("        #            #   #       #       #      #    # #  # #  #  #       ");
+Thread.sleep(76);
+System.out.println("         #    #     #    ####    #       #      #    # #   #   #  ####    ");
+Thread.sleep(76);
+System.out.println("          #  #  #  #     #       #       #      #    # #       #  #       ");
+Thread.sleep(76);             
+System.out.println("           #      #      ######  ####### ###### ###### #       #  ######  ");
+Thread.sleep(76);
+System.out.println("       ");
+
+Thread.sleep(400);
+for(int ij=1;ij<=10;ij++)
+{
+Thread.sleep(100);
+System.out.println(" ");     
 System.out.println();
-System.out.println("                                     TO");
+if(ij==5)
+System.out.println("                                       TO");
+System.out.println();    
+Thread.sleep(80);
+}
+Thread.sleep(100);
+System.out.println("               __ _        ___        _   __       __ _ _ ___ ___     ");
+System.out.println("              |   |_|  /\\   |  |\\ |  |_| |_   /\\  |    |   |  | | |\\ | ");
+System.out.println("              |__ | | /  \\ _|_ | \\|  |\\  |__ /  \\ |__  |  _|_ |_| | \\| ");
+Thread.sleep(800);
+for(int ij=1;ij<=8;ij++)
+{
+System.out.println(" ");
+Thread.sleep(75);
+}
+System.out.println("                        ALL SET TO EXPLODE<<<<<<<<<"+"\n");
+Thread.sleep(1000);
+System.out.println(" ");
+for(int ij=1;ij<=8;ij++)
+{
+System.out.println(" ");
+Thread.sleep(75);
+}
+Thread.sleep(400);
+
+System.out.println("Rules And Regulations--");
+System.out.println("1. There are two bombs red and green and you have to choose any one of them.");
+System.out.println("2. You can choose the grid size and have to enter position where you want your bomb to be placed.");
+System.out.println("3. At corners if there are two bombs of same colour they explode to adjacent sides.");
+System.out.println("4. Bombs in the corners explode in 3 grids and other sides they explode in 4 grids.");
+System.out.println("5. You cannot put bombs on the grids already having opponent's bombs.");
+System.out.println("6. If you have all kinds of colour bombs in the grid you win!!!!");
 System.out.println();
-System.out.println("         __ _        ___        _   __       __ _ _ ___ ___     ");
-System.out.println("        |   |_|  /\\   |  |\\ |  |_| |_   /\\  |    |   |  | | |\\ | ");
-System.out.println("        |__ | | /  \\ _|_ | \\|  |\\  |__ /  \\ |__  |  _|_ |_| | \\| ");
-System.out.println("Rules And Instructions----");
-System.out.println("There are two bombs red and green and you have to chose any one of them");
-System.out.println("you can chose the grid size and have to enter position where u want the bomb to be");
-System.out.println("At corners if there are two bombs of same colour they explode to adjacent sides");
-System.out.println("Atsides the bomb explodes in 3 and in other sides it explodes in 4");
-System.out.println("you cannot put bomb on exsisting opponent bomb");
-System.out.println("If u have all your colour bombs in the grid u win!!!!");
+System.out.println("Game Loading  ");
 System.out.println();
+for(int m1=1;m1<15;m1++)
+{
+Thread.sleep(400);
+System.out.print("*"+" ");
+}
+System.out.println();
+System.out.println("Game running");
+Thread.sleep(800);
 Scanner sc=new Scanner(System.in);
-int n,cut;//declaration
+int n,slic;
 char c3=' ';
 System.out.println("Enter dimension of the square grid");
-n=sc.nextInt();//input 
+n=sc.nextInt();
 String ar[][]=new String[n][n];
 for(int y=0;y<n;y++)
 {
@@ -36,17 +86,17 @@ ar[y][h]="";
 int st[][]=new int[n][n];
 int a[][]=new int[n][n];
 int c=1;
-cut=0;
+slic=0;
 try
 {
 while(true)
 {
-cut++;
+slic++;
 print(ar);
 if(c%2==0)
-System.out.println("Green's Chance enter bomb position");
+System.out.println("Green's Chance: Enter bomb position");
 else
-System.out.println("Red's Chance enter bomb position");
+System.out.println("Red's Chance: Enter bomb position");
 int p1,p2;
 p1=sc.nextInt();
 p2=sc.nextInt();
@@ -112,7 +162,7 @@ ar[p1][p2]="";
 }
 }
 st=new int[n][n];
-if(b==false || cut>(n*100))
+if(b==false || slic>(n*100))
 break;
 }
 int cog=0,cor=0;
@@ -139,9 +189,9 @@ break;
 c++;
 }
 }
-catch(Exception e)
+catch(Exception T)
 {
-System.out.println("Game terminated because of wrong input");
+System.out.println("Game terminated because of invalid input");
 System.exit(0);
 }
 }
@@ -157,17 +207,20 @@ static void print(String ar[][])throws Exception
 for(int y=0;y<ar.length;y++)
 {
 for(int h=0;h<ar[0].length;h++)
-{ if(ar[y][h]=="")
-System.out.printf("%4s","!");
+{
+Thread.sleep(5);
+if(ar[y][h]=="")
+System.out.printf("%5s","!");
 else
-System.out.printf("%4s",ar[y][h]);
+System.out.printf("%5s",ar[y][h]);
 }
 System.out.println();
 }
-}//main function ends
-}//class crs ends
+}                      //End of main method
+}                      //End of class
 
 
 
 
+                   
 
